4b44ca4001d73f16dc742521e4a63c89e00f958e
KYDronePilot <kydronepilotdev@gmail.com>
2020-08-13 23:17:20 -0400

    Got app working in the menu bar
    - Added menu bar icons.
    - Cleaned some stuff up.

--- src/main/main.ts
+++ src/main/main.ts
@@ -25,58 +25,6 @@ let heartbeatHandle: number
 
 Axios.defaults.adapter = require('axios/lib/adapters/http')
 
-// const installExtensions = async () => {
-//     const installer = require('electron-devtools-installer')
-//     const forceDownload = !!process.env.UPGRADE_EXTENSIONS
-//     const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS']
-
-//     return Promise.all([].map(name => installer.default(installer[name], forceDownload)))
-// }
-
-// const createWindow = async () => {
-//     // if (process.env.NODE_ENV !== 'production') {
-//     //     await installExtensions()
-//     // }
-
-//     win = new BrowserWindow({
-//         width: 800,
-//         height: 600,
-//         darkTheme: true,
-//         frame: false,
-//         webPreferences: {
-//             nodeIntegration: true
-//         },
-//         backgroundColor: '#222222'
-//     })
-//     if (process.platform === 'darwin') {
-//         win.setWindowButtonVisibility(false)
-//     }
-
-//     if (process.env.NODE_ENV !== 'production') {
-//         process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = '1' // eslint-disable-line require-atomic-updates
-//         win.loadURL(`http://localhost:2003`)
-//     } else {
-//         win.loadURL(
-//             url.format({
-//                 pathname: path.join(__dirname, 'index.html'),
-//                 protocol: 'file:',
-//                 slashes: true
-//             })
-//         )
-//     }
-
-//     if (process.env.NODE_ENV !== 'production') {
-//         // Open DevTools, see https://github.com/electron/electron/issues/12438 for why we wait for dom-ready
-//         win.webContents.once('dom-ready', () => {
-//             win!.webContents.openDevTools({ mode: 'detach' })
-//         })
-//     }
-
-//     win.on('closed', () => {
-//         win = null
-//     })
-// }
-
 /**
  * Heartbeat function which runs every `HEARTBEAT_INTERVAL` seconds to perform
  * any necessary tasks.
@@ -85,7 +33,14 @@ async function heartbeat() {
     await WallpaperManager.update(Initiator.heartbeatFunction)
 }
 
+const index = url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+})
+
 const mb = menubar({
+    index,
     browserWindow: {
         width: 800,
         height: 600,
@@ -106,14 +61,6 @@ mb.on('after-create-window', () => {
     if (process.env.NODE_ENV !== 'production') {
         process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = '1' // eslint-disable-line require-atomic-updates
         mb.window!.loadURL(`http://localhost:2003`)
-    } else {
-        mb.window!.loadURL(
-            url.format({
-                pathname: path.join(__dirname, 'index.html'),
-                protocol: 'file:',
-                slashes: true
-            })
-        )
     }
 
     if (process.env.NODE_ENV !== 'production') {
@@ -167,18 +114,6 @@ app.on('window-all-closed', () => {
         app.quit()
     }
 })
-
-// app.on('activate', () => {
-//     if (win === null) {
-//         createWindow()
-//     }
-// })
-
-// ipcMain.on(CLOSE_APPLICATION_CHANNEL, () => {
-//     if (win !== undefined) {
-//         win!.close()
-//     }
-// })
 
 ipcMain.on(GET_SATELLITE_CONFIG_CHANNEL, async (event, params: IpcRequest<IpcParams>) => {
     const configStore = SatelliteConfigStore.Instance
