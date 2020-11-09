b2caf2bcaa9ea4ed09719835a97f3691cc05ec53
KYDronePilot <kydronepilotdev@gmail.com>
2020-11-08 22:03:40 -0500

    Fixed issue with license report generator on Windows
    - Needs `.cmd` suffix.

--- generate_license_report.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-const childProcess = require('child_process')
-
-const destPath = path.join(__dirname, 'dist', 'legal_notices.txt')
-
-childProcess.execFile('yarn', ['licenses', 'generate-disclaimer'], (err, stdout, stderr) => {
-    fs.writeFile(destPath, stdout, () => null)
-})
