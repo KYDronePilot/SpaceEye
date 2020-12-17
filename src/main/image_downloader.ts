38a6ec8a67960e559dbf8ec7c690466307662699
KYDronePilot <kydronepilotdev@gmail.com>
2020-12-16 20:48:32 -0500

    Implemented image download progress indicator
    - Only sends IPC updates every 5 percentage points.
    - Uses indeterminate progress bar if the content length cannot be found.

--- src/main/image_downloader.ts
+++ src/main/image_downloader.ts
@@ -23,20 +23,42 @@ const unlinkAsync = promisify(Fs.unlink)
  * @param url - URL to download
  * @param destPath - Destination file path to save to
  * @param cancelToken - Cancel token
+ * @param onProgress - Called whenever the percentage downloaded value is updated
  */
 async function downloadStream(
     url: string,
     destPath: string,
-    cancelToken: CancelTokenSource
+    cancelToken: CancelTokenSource,
+    onProgress: (percentage?: number) => void
 ): Promise<void> {
     return new Promise<void>((resolve, reject) => {
         Axios.get(url, {
             responseType: 'stream',
             cancelToken: cancelToken.token
         })
-            .then(({ data }) => {
+            .then(({ data, headers }) => {
                 const writer = Fs.createWriteStream(destPath)
                 const dataStream = data as Readable
+                // Get the content size (if available)
+                const contentLength = parseInt((headers ?? {})['content-length'] ?? '-1', 10)
+                // If none found, signal that the download has begun indeterminately
+                if (contentLength === -1) {
+                    onProgress(-1)
+                } else {
+                    // If length found, set up to send percentage updates
+                    let lengthDownloaded = 0
+                    let previousPercentage = -1
+                    // TODO: NOT TYPESAFE!!!!!!!!!
+                    dataStream.on('data', chunk => {
+                        lengthDownloaded += chunk.length
+                        const percentage = Math.round((lengthDownloaded / contentLength) * 100)
+                        // Only send an update if a percentage point changed
+                        if (percentage !== previousPercentage) {
+                            previousPercentage = percentage
+                            onProgress(percentage)
+                        }
+                    })
+                }
                 dataStream.pipe(writer)
                 let isCancelled = false
                 cancelToken.token.promise.then(async cancellation => {
@@ -51,6 +73,8 @@ async function downloadStream(
                     }
                 })
                 writer.on('close', () => {
+                    // Signal that we are done downloading
+                    onProgress(undefined)
                     if (!isCancelled) {
                         resolve()
                     }
@@ -75,20 +99,22 @@ async function downloadStream(
  * @param image - Image to download
  * @param cancelToken - Cancel token for this download
  * @param lock - Active lock on the download pipeline
+ * @param onProgress - Called whenever the percentage downloaded value is updated
  * @throws {RequestCancelledError} If request is cancelled
  * @returns Downloaded image information
  */
 export async function downloadImage(
     image: ImageSource,
     cancelToken: CancelTokenSource,
-    lock: UpdateLock
+    lock: UpdateLock,
+    onProgress: (percentage?: number) => void
 ): Promise<DownloadedImage> {
     // FIXME: Don't leave as hardcoded jpg
     const downloadedImage = new DownloadedImage(image.id, moment.utc(), 'jpg')
     log.debug('Downloading image to:', downloadedImage.getPath())
 
     try {
-        await downloadStream(image.url, downloadedImage.getPath(), cancelToken)
+        await downloadStream(image.url, downloadedImage.getPath(), cancelToken, onProgress)
         lock.destroyCancelToken(cancelToken)
     } catch (error) {
         lock.destroyCancelToken(cancelToken)
