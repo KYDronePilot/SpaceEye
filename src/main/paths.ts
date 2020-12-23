34afcef3f163577915416094a91413f0eaba580a
KYDronePilot <kydronepilotdev@gmail.com>
2020-12-22 20:53:26 -0500

    Added special handling when setting background in UWP builds
    - Fixes repeated store review rejections.
    - UWP (Windows Store) builds translate AppData folder filesystem
      requests to a virtual folder location.
      - win32 API doesn't know about these locations, so the actual (real)
        paths must be used when making the call.
      - Before calling library to set the background, look for image in
        potential virtual directories and use them instead.

--- src/main/paths.ts
+++ src/main/paths.ts
@@ -4,8 +4,10 @@
 
 import { app } from 'electron'
 import Fs from 'fs'
+import { sync as globSync } from 'glob'
 import Path from 'path'
 
+const APP_DATA_DIR = app.getPath('appData')
 const USER_DATA_DIR = app.getPath('userData')
 const imagesDir = Path.join(USER_DATA_DIR, 'downloaded_images')
 
@@ -15,3 +17,11 @@ if (!Fs.existsSync(imagesDir)) {
 }
 
 export const IMAGES_DIR = imagesDir
+
+// Alternate "non-virtualized" locations of downloaded image files for UWP (Windows Store) builds
+export const UWP_IMAGE_DIRS =
+    process.windowsStore === true
+        ? globSync(
+              `${APP_DATA_DIR}/../Local/Packages/43181KYDronePilot.SpaceEye_*/LocalCache/Roaming/space-eye/downloaded_images`
+          )
+        : []
